81f2efaf04a64606cc4d267b6723c84c05c7a918
Redian23 <vasya.degski@gmail.com>
2021-02-15 15:47:34 +0300

    Add .zhsrc for manj-conf

--- arch.sh
+++ arch.sh
@@ -17,27 +17,32 @@ do
   case $INPUT_STRING in
 	1)
 		sudo pacman -S audacious celluloid gthumb evince foliate
+		continue
 		;;
 	2)
 		sudo pacman -S kdenlive 
-		break
+		continue
 		;;
 	3)
-		sudo pacman -S gparted virtualbox engrampa lxtask gnome-screenshot manjaro-zsh-config pantheon-calculator
-        sudo pacman -S pacaur 
+		sudo pacman -S gparted virtualbox engrampa lxtask gnome-screenshot pantheon-calculator
+		sudo pacman -S pacaur 
         sudo pacaur -S virtualbox-ext-oracle
+		sudo pacaur -S manjaro-zsh-config && cp .zshrc ~/
+		continue
 		;;
 
 	4)
 		sudo pacman -S  transmission-gtk vivaldi
+		continue
 		;;
     5)
 		flatpak install flathub org.onlyoffice.desktopeditors
+		continue
         ;;
 	*)
 		echo "Sorry, I don't understand"
+		break
         ;;
   esac
 done
-echo 
 echo "Thx"
