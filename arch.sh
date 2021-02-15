65a1f7c34268c7e2f2277dd5f86f3553ef9a8222
Redian23 <vasya.degski@gmail.com>
2021-02-15 16:36:59 +0300

    Add new app on script

--- arch.sh
+++ arch.sh
@@ -4,10 +4,10 @@ echo "Someone app need a AUR repository!"
 echo "List of install app: "
 echo "
 1.Audio & Video complect (Audacious,celluloid, gthumb)
-2.Edit complect (Kdenlive)
+2.Internet complect (transmission-gtk, vivaldi)
 3.Utilit complect (Gparted, VirtualBox, engrampa, lxtask, gnome-screenshot, manjaro-zsh-config, pantheon-calculator, popsicle) 
-4.Internet complect (transmission-gtk, vivaldi) 
-5.Office (onlyoffice, evince, foliate)
+4.Video/Audo Edit complect (Kdenlive,audacity)
+5.Document edit complect (onlyoffice, evince, foliate, visual-studio-code-bin)
 6.Exit
 "
 
@@ -16,21 +16,21 @@ do
   read INPUT_STRING
   case $INPUT_STRING in
 	1)
-		sudo pacman -S audacious 
-		sudo pacman -S celluloid gthumb 
+		sudo pacman -S audacious celluloid gthumb 
 		echo " " 
 		echo "Select other item on list :"
 		continue
 		;;
 	2)
-		sudo pacman -S kdenlive 
-
+		sudo pacman -S  transmission-gtk vivaldi
+		echo " " 
+		echo "Select other item on list :"
 		continue
 		;;
+
 	3)
-		sudo pacman -S gparted virtualbox engrampa lxtask gnome-screenshot pantheon-calculator popsicle 
-		sudo pacman -S pacaur 
-		sudo pacaur -S virtualbox-ext-oracle
+		sudo pacman -S pacaur gparted virtualbox engrampa lxtask gnome-screenshot pantheon-calculator popsicle 
+		pacaur -S virtualbox-ext-oracle
 		sudo pacman -S manjaro-zsh-config && cp .zshrc ~/
 		echo " " 
 		echo "Select other item on list :"
@@ -38,13 +38,12 @@ do
 		;;
 
 	4)
-		sudo pacman -S  transmission-gtk vivaldi
-		echo " " 
-		echo "Select other item on list :"
+		sudo pacman -S kdenlive audacity
 		continue
 		;;
 	5)
 		sudo pacman -S evince foliate
+		pacaur -S visual-studio-code-bin
 		flatpak install flathub org.onlyoffice.desktopeditors
 		echo " " 
 		echo "Select other item on list :"
