ab8b97d775b1f3085ffdbc9295fdf3c9c2bcc00d
Redian23 <vasya.degski@gmail.com>
2021-02-15 15:59:48 +0300

    Edit items on List app

--- arch.sh
+++ arch.sh
@@ -3,12 +3,12 @@ echo "Someone app need a AUR repository!"
 
 echo "List of install app: "
 echo "
-1.Audio & Video & Document complect (Audacious,celluloid, gthumb, evince, foliate)
+1.Audio & Video complect (Audacious,celluloid, gthumb)
 2.Edit complect (Kdenlive)
-3.Utilit complect (Gparted, VirtualBox, engrampa, lxtask, gnome-screenshot, manjaro-zsh-config, pantheon-calculator) 
+3.Utilit complect (Gparted, VirtualBox, engrampa, lxtask, gnome-screenshot, manjaro-zsh-config, pantheon-calculator, popsicle) 
 4.Internet complect (transmission-gtk, vivaldi) 
-5.Office (onlyoffice)
-6+.Exit
+5.Office (onlyoffice, evince, foliate)
+6.Exit
 "
 
 while :
@@ -16,33 +16,44 @@ do
   read INPUT_STRING
   case $INPUT_STRING in
 	1)
-		sudo pacman -S audacious celluloid gthumb evince foliate
+		sudo pacman -S audacious 
+		sudo pacman -S celluloid gthumb 
+		echo " " 
+		echo "Select other item on list :"
 		continue
 		;;
 	2)
 		sudo pacman -S kdenlive 
+
 		continue
 		;;
 	3)
-		sudo pacman -S gparted virtualbox engrampa lxtask gnome-screenshot pantheon-calculator
+		sudo pacman -S gparted virtualbox engrampa lxtask gnome-screenshot pantheon-calculator popsicle 
 		sudo pacman -S pacaur 
 		sudo pacaur -S virtualbox-ext-oracle
-		sudo pacaur -S manjaro-zsh-config && cp .zshrc ~/
+		sudo pacman -S manjaro-zsh-config && cp .zshrc ~/
+		echo " " 
+		echo "Select other item on list :"
 		continue
 		;;
 
 	4)
 		sudo pacman -S  transmission-gtk vivaldi
+		echo " " 
+		echo "Select other item on list :"
 		continue
 		;;
 	5)
+		sudo pacman -S evince foliate
 		flatpak install flathub org.onlyoffice.desktopeditors
+		echo " " 
+		echo "Select other item on list :"
 		continue
 		;;
 	*)
-		echo "Sorry, I don't understand"
+		echo "You are select EXIT"
 		break
-        ;;
+		;;
   esac
 done
 echo "Thx"
